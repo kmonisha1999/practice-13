de33967d82f97d301d358782b50ebcc7a028b31c
kmonisha1999 <monisha291099@gmail.com>
2022-10-31 23:17:49 +0530

    js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+
+
+let a=10;
+console.log(++a);
+console.log(a);
+
+
+ let b=10;
+ console.log(b++);
+ console.log(b);
+
+ let x=5, y=3;
+ console.log(x+y);
+
+ let num1=5, num2=3;
+ console.log(num1-num2);
+
+ let m=5, n=3;
+ console.log(m/n);
+
+ let c1=5, c2=3;
+ console.log(c1%c2);
+
+ let z1=5, z2=3;
+ console.log(z1**z2);
+
+ let age=20
+ let marriage=age>=21 ? "eligible":"not eligible";
+ console.log(marriage)
